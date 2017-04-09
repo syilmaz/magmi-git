b0d2c56693eb004df6aa3334f1096e3e63dd6ddb
Samet Yilmaz <syilmaz@spicedcoding.com>
2017-04-09 11:27:48 +0200

    Add unit tests

--- magmi/tests/unit/tests/engines/magmi_productimportengine.test.php
+++ magmi/tests/unit/tests/engines/magmi_productimportengine.test.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__ . "/../../../inc/magmi_defs.php");
-require_once(__DIR__ . "/../../../inc/magmi_statemanager.php");
+require_once(__DIR__ . "/../../../../inc/magmi_defs.php");
+require_once(__DIR__ . "/../../../../inc/magmi_statemanager.php");
 
 require_once MAGMI_INCDIR . '/magmi_config.php';
 require_once MAGMI_PLUGIN_DIR . '/inc/magmi_item_processor.php';
@@ -12,12 +12,14 @@ class MagmiProductImportEngineTest extends \PHPUnit\Framework\TestCase
     {
         $object = new Magmi_ProductImportEngine();
 
-        $this->assertEquals(array(
+        $this->assertEquals(
+            array(
                 "itemprocessors" => array(
                     "Magmi_DefaultAttributeItemProcessor"
                 )
             ),
-            $object->getBuiltinPluginClasses());
+            $object->getBuiltinPluginClasses()
+        );
     }
 
 }
